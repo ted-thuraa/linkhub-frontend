5ef7b968b3d40e823dd0ada8b470525b3f144a0f
ted-thuraa <mutwirited@gmail.com>
2023-05-22 09:26:18 +0300

    first commit

---
+++ layouts/AuthLayouts.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-lg bg-orange-500">auth layout</div>
+</template>
+
+<script setup></script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <AuthLayout></AuthLayout>
+</template>
+
+<script setup>
+import AuthLayout from "~/layouts/AuthLayouts.vue";
+</script>
